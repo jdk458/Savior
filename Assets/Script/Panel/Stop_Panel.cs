9fa44028acff91e32d26982e80ee16a31f36d2b9
jdk458 <jdk458@naver.com>
2020-05-18 00:57:00 +0900

    0518

---
+++ Assets/Script/Panel/Stop_Panel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stop_Panel : MonoBehaviour
+{
+    public GameObject Stop_panel;
+    public GameObject black_panel;
+
+    private void Awake()
+    {
+        Stop_panel.SetActive(false);
+    }
+
+    public void Pop_Panel()
+    {
+        //게임멈춤
+        Stop_panel.SetActive(true);
+        black_panel.SetActive(true);
+    }
+
+    public void OnClick_Back()
+    {
+        //게임멈춤 풀기
+        Stop_panel.SetActive(false);
+        black_panel.SetActive(false);
+    }
+    public void OnClick_Out()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
